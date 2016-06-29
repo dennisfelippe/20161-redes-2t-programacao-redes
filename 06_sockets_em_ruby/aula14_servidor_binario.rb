d826638b3f3a7812efa354046ae9961d14f050d5
Dennis Felippe <zero_dfvn@Hotmail.com>
2016-06-29 19:43:14 -0300

    Proposta de Solução
    Proposta de Solução Para o Unpack no Read Nulo

--- 06_sockets_em_ruby/aula14_servidor_binario.rb
+++ 06_sockets_em_ruby/aula14_servidor_binario.rb
@@ -3,20 +3,23 @@ serverSocket = TCPServer.new(2000)
 begin
   socket = serverSocket.accept
   loop{
-    byte_um = socket.read(1).unpack('C')[0]
-    if byte_um == 0x00
-      campos = socket.read(2).unpack('CC')
-      soma = campos[0] + campos[1]
-      out = [soma].pack('n')
-    elsif byte_um == 0x01
-      byte_dois = socket.read(1).unpack('C')[0]
-      quadrado = byte_dois * byte_dois
-      out = [quadrado].pack('n')
-    else
-      out = [0xFFFF].pack('n')
-    end
-    socket.syswrite(out)
-  }
+	 byte_um = socket.read(1).unpack('C')[0]
+	if byte_um == 0x00
+	 campos = socket.read(2).unpack('CC')
+	 soma = campos[0] + campos[1]
+	 out = [soma].pack('n')
+	elsif byte_um == 0x01
+	 byte_dois = socket.read(1).unpack('C')[0]
+	 quadrado = byte_dois * byte_dois
+	 out = [quadrado].pack('n')
+	else
+	 out = [0xFFFF].pack('n')
+	end
+	 socket.syswrite(out)
+	}
+rescue
+retry
+
 ensure
-	serverSocket.close  
+	serverSocket.close
 end
